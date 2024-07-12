138fd474f98bed79aa72cfd23c4d55e32b6fa880
Vinul Vinsura <vinulviksura16@gmail.com>
2024-07-12 23:42:07 +0530

    creat JwtService class

--- Passenger-Service/src/main/java/com/example/passengerservice/entity/Passenger.java
+++ Passenger-Service/src/main/java/com/example/passengerservice/entity/Passenger.java
@@ -4,13 +4,17 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "Passenger")
-public class Passenger {
+public class Passenger implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -21,4 +25,33 @@ public class Passenger {
     private String address;
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- Passenger-Service/src/main/java/com/example/passengerservice/service/Impl/PassengerImpl.java
+++ Passenger-Service/src/main/java/com/example/passengerservice/service/Impl/PassengerImpl.java
@@ -1,6 +1,8 @@
 package com.example.passengerservice.service.Impl;
 
 import com.example.passengerservice.service.PassengerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PassengerImpl implements PassengerService {
 }
