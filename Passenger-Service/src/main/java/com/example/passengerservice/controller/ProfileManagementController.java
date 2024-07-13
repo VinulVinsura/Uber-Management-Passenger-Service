958770939faba340eea157d871b933d0324a2bf4
Vinul Vinsura <vinulviksura16@gmail.com>
2024-07-13 10:55:13 +0530

    Set JWT-Spring-Security

---
+++ Passenger-Service/src/main/java/com/example/passengerservice/controller/ProfileManagementController.java
@@ -0,0 +1,14 @@
+package com.example.passengerservice.controller;
+
+import com.example.passengerservice.dto.PassengerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class ProfileManagementController {
+
+
+}
--- Passenger-Service/src/main/java/com/example/passengerservice/repository/PassengerRepo.java
+++ Passenger-Service/src/main/java/com/example/passengerservice/repository/PassengerRepo.java
@@ -3,5 +3,9 @@ package com.example.passengerservice.repository;
 import com.example.passengerservice.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PassengerRepo extends JpaRepository<Passenger,Integer> {
+
+    Optional<Passenger> findByEmail(String email);
 }
