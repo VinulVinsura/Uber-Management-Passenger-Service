160f458adff3f8737148211ece8deed1b0e14d7c
Vinul Vinsura <vinulviksura16@gmail.com>
2024-07-15 22:15:25 +0530

    creat packages

--- Uber-System-Service/src/main/java/com/example/ubersystemservice/controller/NotificationController.java
+++ Uber-System-Service/src/main/java/com/example/ubersystemservice/controller/NotificationController.java
@@ -1,4 +1,10 @@
 package com.example.ubersystemservice.controller;
 
+import ch.qos.logback.classic.spi.ConfiguratorRank;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class NotificationController {
 }
